0ff6e5a48e19dfdd793350fed86c3784258f206a
Ahmed Lotfy <45941854+Alotfy55@users.noreply.github.com>
2020-04-10 23:24:58 +0200

    Project as rectangles

--- core/src/com/angrybirds/game/Bird.java
+++ core/src/com/angrybirds/game/Bird.java
@@ -4,19 +4,12 @@ import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-/**
+import com.badlogic.gdx.physics.box2d.World;
+
 public class Bird extends Everything {
 
-    public Bird(Vector2 midPoint) {
-        imgPath = new String[2];
-        health = 100 ;
-        height = 40 ;
-        width = 40 ;
-        weight = 10;
-        this.x = midPoint.x ;
-        this.y = midPoint.y;
-        this.Velocity = new Vector2(0,0);
-        this.BounceValue = 0.69f;
+    public Bird(World world, int x , int y , int width , int height , boolean isStatic) {
+		super( world,  x ,  y ,  width ,  height ,  isStatic);
     }
     public int getCondition() {
         if (health > 50)
@@ -24,4 +17,3 @@ public class Bird extends Everything {
         else return 1;
     }
 }
- */
--- core/src/com/angrybirds/game/game.java
+++ core/src/com/angrybirds/game/game.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.physics.box2d.*;
+import org.graalvm.compiler.virtual.phases.ea.PartialEscapeClosure;
 
 import javax.swing.*;
 import java.awt.event.KeyEvent;
@@ -17,172 +18,114 @@ import java.util.Random;
 
 public class game extends ApplicationAdapter {
 		SpriteBatch batch;
-		Texture img ;
+
+		Texture img;
 		Texture Birds;
-		Texture background ;
+		Texture background;
+
 		Sprite test2;
 		Sprite test;
 		Sprite test1;
-		/**final float Gravity =9.8f;
-	Everything[] levelObjects;
-	boolean hold; // to determine if mouse button is held down
-	boolean pausePhysics; // to start physics when the mouse button is released
-	int NumOfObjects;
-	boolean [][] collided;
-	int frameCounter = 0;*/
-		int NumOfObjects;
-		World world ;
+
+
+		public World world;
 		Box2DDebugRenderer dDebugRenderer;
-		OrthographicCamera camera ;
-		Body player , player1 [] , bedRock;
-
-	@Override
-	public void create () {
-		Box2D.init();
-		world = new World(new Vector2(0 , -10),false);
-		dDebugRenderer = new Box2DDebugRenderer();
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false , Gdx.graphics.getWidth() /2 , Gdx.graphics.getHeight()/2);
-		player = createPlayer(300,300,20,20 , false);
-
-		NumOfObjects = 10;
-		batch = new SpriteBatch();
-		img = new Texture("BB.png");
-		background = new Texture("background.png");
-		Birds = new Texture("RED.png");
-		test2 = new Sprite(Birds);
-		test = new Sprite(img);
-		test1 = new Sprite(background);
-		player1 = new Body[NumOfObjects];
-
-		for (int i = 1 ; i < NumOfObjects ; i++)
-		{
-				player1[i] = createPlayer(500 + i*10,50 + i *50 ,20 ,20 , false );
-		}
-		player1[0] = createPlayer(500 , 50 , 20 , 20 , true);
-
-		/**levelObjects = new Everything[NumOfObjects];
-		levelObjects[0] = new Bird(new Vector2(150,200));
-		int h = 30;
-
-		for (int i = 1 ; i < NumOfObjects ; i++)
-		{
-			h+=70;
-			levelObjects[i] = new Bird(new Vector2(500 , h));
-			double velx = 0; // Math.random()*10;
-			levelObjects[i].Velocity.x = (float) velx;
-		}
-
-		hold = false;
-		pausePhysics = true;
-		test.setPosition(levelObjects[0].x,levelObjects[0].y);
-		test1.setPosition(0,-130);
-
-		collided = new boolean[NumOfObjects][NumOfObjects];
-		for ( int i = 0 ; i < NumOfObjects ; i++)
-		{
-			collided[i] = new boolean[NumOfObjects];
-		}
-*/
-
-		bedRock = createPlayer(Gdx.graphics.getWidth()/2 ,90 , 1000,10,true );
-	}
-
-	@Override
-	public void render () {
-		world.step(1/10f,6,2);
+		OrthographicCamera camera;
 
 
-		Gdx.gl.glClearColor(135, 206, 236, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		/**Vector2 renderPoint = levelObjects[0].getMidPoint();
+		int NumOfObjects;
+		Everything player, player1[], bedRock;
+
+		boolean pausePhysics;
+		boolean hold;
+		public static final float PPM = 32;
+		public Vector2 mouseOrigin;
+		final int MouseLimit = 50;
+		@Override
+		public void create() {
+				Box2D.init();
+				world = new World(new Vector2(0, -10), false);
+				dDebugRenderer = new Box2DDebugRenderer();
+				camera = new OrthographicCamera();
+				camera.setToOrtho(false, (float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2);
+				pausePhysics = true;
+				hold = false;
+
+				NumOfObjects = 10;
+				batch = new SpriteBatch();
+				img = new Texture("BB.png");
+				background = new Texture("background.png");
+				Birds = new Texture("RED.png");
+				test2 = new Sprite(Birds);
+				test = new Sprite(img);
+				test1 = new Sprite(background);
 
 
+				player1 = new Bird[NumOfObjects];
+				player = new Bird(world, 150, 300, 20, 20, false);
 
-		batch.begin();
-		batch.draw(test1,test1.getX(),test1.getY() ,1280 , 400 ); // draw background
-
-		batch.draw(test2,test.getX(),test.getY() , levelObjects[0].width , levelObjects[0].height); // draw first bird
-
-		for ( int i = 1 ; i < NumOfObjects ; i++)
-		{
-			batch.draw(test,levelObjects[i].getX(),levelObjects[i].getY() , levelObjects[i].width , levelObjects[i].height); // draw other birds
-			//if (!pausePhysics)
-			//levelObjects[i].applyPhysics(Gravity);
-		}
-
-		//batch.draw(test1,test1.getX(),test1.getY() , 0,0); // draw second bird
-
-		if (!pausePhysics) // apply bird physics
-		{
-			test.setPosition(renderPoint.x, renderPoint.y);
-			//levelObjects[0].applyPhysics(Gravity);
-
-		}
-		//inCollision(levelObjects);
-		batch.end();
-
-
-		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)  && levelObjects[0].includes(Gdx.input.getX(),Gdx.graphics.getHeight() -Gdx.input.getY()) && pausePhysics) {
-			levelObjects[0].setVelocity(new Vector2(5 ,5));
-
-			test.setPosition(Gdx.input.getX() - test.getWidth()/2,
-					Gdx.graphics.getHeight() - Gdx.input.getY() - test.getHeight()/2);
-
-			hold = true;
-		}
-
-		if (hold){
-			test.setPosition(Gdx.input.getX() - 25,
-					Gdx.graphics.getHeight() - Gdx.input.getY() -25);
-			if (!Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-
-				hold = false ;
-
-				levelObjects[0].setVelocity(new Vector2(-(test.getX() - levelObjects[0].x) /10, ( levelObjects[0].y - test.getY())/8));
-
-				levelObjects[0].setCenter(test.getX() , test.getY());
-
-				pausePhysics = false;
-
-			}
-		}
-
-		/**if (frameCounter%10 == 0)
-		{
-			for(int i = 0 ; i < NumOfObjects ; i++)
-			{
-				for (int j = 0 ; j < NumOfObjects ; j++)
-				{
-					collided[i][j]=false;
+				for (int i = 0; i < NumOfObjects; i++) {
+						player1[i] = new Bird(world, 700 + i * 10, 200 + i * 50, 20, 20, false);
 				}
-			}
-			frameCounter = 0;
-			frameCounter++;
-		}*/
+				bedRock = new Bird(world, Gdx.graphics.getWidth() / 2, 90, 1000, 10, true);
+		}
 
-		batch.begin();
-		batch.draw(test1 , 0,0,1280,300);
-			for ( int i = 0 ; i < NumOfObjects ; i++)
-			{
-					batch.draw(test, player1[i].getPosition().x, player1[i].getPosition().y , 50 , 50); // draw other birds
-					//if (!pausePhysics)
-					//levelObjects[i].applyPhysics(Gravity);
-			}
-		batch.draw(Birds ,player.getPosition().x, player.getPosition().y,50 ,50);
-		batch.end();
-		//dDebugRenderer.render(world,camera.combined);
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-		Birds.dispose();
-		background.dispose();
-		world.dispose();
-		dDebugRenderer.dispose();
-	}
+		@Override
+		public void render() {
+				if (!pausePhysics)
+					world.step(1 / 60f, 6, 2);
+
+
+				Gdx.gl.glClearColor(135, 206, 236, 1);
+				Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+				batch.begin();
+
+				batch.draw(test1, 0, 0, 1280, 300);
+
+				for (int i = 0; i < NumOfObjects; i++) {
+						batch.draw(test, player1[i].body.getPosition().x * PPM, player1[i].body.getPosition().y * PPM, 50, 50); // draw other birds
+				}
+
+				if ( Gdx.input.getPressure() == 1 && pausePhysics)
+				{
+						hold =true;
+						if (mouseOrigin == null)
+						mouseOrigin = new Vector2(Gdx.input.getX() ,Gdx.graphics.getHeight() -  Gdx.input.getY());
+				}
+				if (hold)
+				{
+						getMouseInput();
+						if (Math.abs(mouseOrigin.x - Gdx.input.getX()) <= MouseLimit )
+								test2.setPosition(Gdx.input.getX() , test2.getY());
+						if (Math.abs(mouseOrigin.y - Gdx.input.getY()) <= MouseLimit )
+								test2.setPosition(test2.getX() , Gdx.graphics.getHeight() - Gdx.input.getY());
+
+						batch.draw(test2, test2.getX(),test2.getY(),50 , 50 ); //player.getPosition().x*PPM, player.getPosition().y*PPM
+				}
+				if (!hold)
+				batch.draw(test2, player.body.getPosition().x * PPM, player.body.getPosition().y * PPM ,50 , 50 ); //player.getPosition().x*PPM, player.getPosition().y*PPM
+				batch.end();
+
+				System.out.println(test2.getY()+ " " + (Gdx.graphics.getHeight() - Gdx.input.getY()) +" "+ player.body.getPosition().x*PPM +" "+ Gdx.input.getX() );
+
+
+
+
+				dDebugRenderer.render(world, camera.combined.scl(PPM));
+				camera.combined.scl(1 / PPM);
+		}
+
+		@Override
+		public void dispose() {
+				batch.dispose();
+				img.dispose();
+				Birds.dispose();
+				background.dispose();
+				world.dispose();
+				dDebugRenderer.dispose();
+		}
+
 /**
 	public void inCollision(Everything[] levelObjects)
 	{
@@ -201,22 +144,33 @@ public class game extends ApplicationAdapter {
 	}
 */
 
-public Body createPlayer(int x , int y , int width , int height , boolean isStatic) {
-		Body pBody;
-		BodyDef def = new BodyDef();
-		if(isStatic)
-				def.type = BodyDef.BodyType.StaticBody;
-		else
-			def.type = BodyDef.BodyType.DynamicBody;
-		def.position.set(x,y);
-		def.fixedRotation = false ;
-		pBody = world.createBody(def);
+public void getMouseInput()
+{
 
-		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(width,height);
+		if (includes((Bird) player , new Vector2(Gdx.input.getX(),Gdx.graphics.getHeight() - Gdx.input.getY() )) && Gdx.input.isButtonPressed(Input.Buttons.LEFT)  )
+		{
+				player.body.setTransform((float) Gdx.input.getX()/32 ,(float) (Gdx.graphics.getHeight()- Gdx.input.getY())/32,0);
+		}
+		if(Gdx.input.getPressure() == 0)
+		{
+				player.body.setTransform(test2.getX()/32 , test2.getY()/32,0);
+				hold=false;
+				pausePhysics = false;
+				player.body.setLinearVelocity( ((mouseOrigin.x - Gdx.input.getX()) /5 ) , (mouseOrigin.y - (Gdx.graphics.getHeight() - Gdx.input.getY()))/5);
 
-		pBody.createFixture(shape,  1.0f);
-		shape.dispose();
-		return pBody;
-	}
+		}
 }
+
+
+
+	public boolean includes(Bird player,Vector2 mousePoint )
+	{
+			if (mousePoint.x > player.body.getPosition().x*PPM - (player.width) && mousePoint.x < player.body.getPosition().x*PPM + (player.width) &&
+			mousePoint.y > player.body.getPosition().y*PPM - (player.height)  && mousePoint.y < player.body.getPosition().y*PPM + (player.height))
+					return true;
+			else
+					return false;
+	}
+
+}
+ ////  solve mouse controls
